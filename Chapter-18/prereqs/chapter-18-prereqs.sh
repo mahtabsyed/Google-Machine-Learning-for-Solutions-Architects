425fb4cff9f772ace43ec16179bd0df354fe3e23
Mahtab Syed <mahtab.syed@gmail.com>
2024-08-03 15:49:03 +1000

    first commit

---
+++ Chapter-18/prereqs/chapter-18-prereqs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the project number of our current project:
+export PROJECT_NUMBER=`gcloud projects describe $GOOGLE_CLOUD_PROJECT --format='value(projectNumber)'`
+
+# Grant the "Eventarc Event Receiver" IAM role to your project's default Compute Engine service account:
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com --role=roles/eventarc.eventReceiver
+
+# Grant the "Document AI API User" IAM role to your project's default Compute Engine service account:
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com --role="roles/documentai.apiUser"
+
+# Grant the "Service Account Token Creator" IAM role to the Pub/Sub service agent for your project:
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com --role=roles/iam.serviceAccountTokenCreator
+
+# Grant the "Pub/Sub Publisher" IAM role to the GCS service agent for your project:
+export GCS_SA=`gsutil kms serviceaccount -p $GOOGLE_CLOUD_PROJECT`
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member="serviceAccount:${GCS_SA}" --role='roles/pubsub.publisher'
---
+++ README.md
@@ -0,0 +1,16 @@
+# Google Machine Learning and Generative AI for Solutions Architects
+
+**Build efficient and scalable AI/ML solutions on Google Cloud**
+## Learning on Google Cloud
+* Build solutions with open-source offerings on Google Cloud, such as TensorFlow, PyTorch, and Spark
+* Source, understand, and prepare data for ML workloads
+* Build, train, and deploy ML models on Google Cloud
+* Create an effective MLOps strategy and implement MLOps workloads on Google Cloud
+* Discover common challenges in typical AI/ML projects and get solutions from experts
+* Explore vector databases and their importance in Generative AI applications
+* Uncover new Gen AI patterns such as Retrieval Augmented Generation (RAG), agents, and agentic workflows 
+
+## Refer
+<a href="https://www.packtpub.com/en-in/product/google-machine-learning-and-generative-ai-for-solutions-architects-9781803245270"><img src="https://m.media-amazon.com/images/I/81Xj95chYRL._SL1500_.jpg" alt="Google Machine Learning and Generative AI for Solutions Architects" height="256px" align="right"></a>
+
+This is the code repository for [Google Machine Learning and Generative AI for Solutions Architects](https://www.packtpub.com/en-in/product/google-machine-learning-and-generative-ai-for-solutions-architects-9781803245270), published by Packt.
